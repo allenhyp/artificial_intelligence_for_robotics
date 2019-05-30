e4da6113285de9c726c97ebdba7e1d88aa4179fd
bboypupu <bboypupu@gmail.com>
2019-05-29 23:43:06 -0700

    PD controller implementation

--- 016_PID_controller.py
+++ 016_PID_controller.py
@@ -1,14 +1,16 @@
 # -----------
 # User Instructions
 #
-# Implement a P controller by running 100 iterations
-# of robot motion. The desired trajectory for the 
-# robot is the x-axis. The steering angle should be set
-# by the parameter tau so that:
+# Implement a PD controller by running 100 iterations
+# of robot motion. The steering angle should be set
+# by the parameter tau_p and tau_d so that:
 #
-# steering = -tau * crosstrack_error
+# steering = -tau_p * CTE - tau_d * diff_CTE
+# where differential crosstrack error (diff_CTE)
+# is given by CTE(t) - CTE(t-1)
 #
-# You'll only need to modify the `run` function at the bottom.
+#
+# Only modify code at the bottom! Look for the TODO
 # ------------
  
 import random
@@ -99,22 +101,39 @@ class Robot(object):
 # ------------------------------------------------------------------------
 #
 # run - does a single control run
-robot = Robot()
-robot.set(0.0, 1.0, 0.0)
 
-def run(robot, tau, n=100, speed=1.0):
+# previous P controller
+def run_p(robot, tau, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
-    # TODO: your code here
-    for t in range(n):
-        robot.move(-tau * robot.y, speed) # Just use the speed as the distance per cycle here
+    for i in range(n):
+        cte = robot.y
+        steer = -tau * cte
+        robot.move(steer, speed)
         x_trajectory.append(robot.x)
         y_trajectory.append(robot.y)
     return x_trajectory, y_trajectory
     
-x_trajectory, y_trajectory = run(robot, 0.1)
+robot = Robot()
+robot.set(0, 1, 0)
+
+def run(robot, tau_p, tau_d, n=100, speed=1.0):
+    x_trajectory = []
+    y_trajectory = []
+    # TODO: your code here
+    last_cte = robot.y
+    for i in range(n):
+        cte = robot.y
+        steer = -tau_p * cte - tau_d * (cte - last_cte)
+        robot.move(steer, speed)
+        x_trajectory.append(robot.x)
+        y_trajectory.append(robot.y)
+        last_cte = cte
+    return x_trajectory, y_trajectory
+    
+x_trajectory, y_trajectory = run(robot, 0.2, 3.0)
 n = len(x_trajectory)
 
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-ax1.plot(x_trajectory, y_trajectory, 'g', label='P controller')
+ax1.plot(x_trajectory, y_trajectory, 'g', label='PD controller')
 ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
